c716ef37dd5904a8081caaa89b5c0a0fc7bca684
Dowhyj <100160697+Dowhyj@users.noreply.github.com>
2024-06-22 16:05:49 -0300

    Complejidades

--- src/main/java/aed/SistemaSIU.java
+++ src/main/java/aed/SistemaSIU.java
@@ -14,7 +14,12 @@ public class SistemaSIU {
     }
     Carreras carreras;
     Estudiante estudiantes;
+  
     //1
+    //En el bucle doble se recorren todas las 'infoMaterias' y para cada una se recorren sus 'ParCarreraMateria' (primera sumatoria). En esta se realizan operaciones de búsqueda e inserción que son direcatamente propocionales a la longitud de la cadena de caracteres
+    //La segunda sumatoria se realiza en el for dentro del primer for que depende de la longitud de los nombres de la materia
+    //La inserción de estudiante es directamente proporcional a la cantidad total de Estudiantes
+
     public SistemaSIU(InfoMateria[] infoMaterias, String[] libretasUniversitarias){
         carreras = new Carreras();   
         estudiantes = new Estudiante();
@@ -40,42 +45,74 @@ public class SistemaSIU {
         }
 
     }
+    
     //2
+    //En el peor de los casos, se recorre hasta la ultima carrera dentro trie carrera, y luego hasta la ultima la ultima de esa carrera. 
+    //Las operaciones de busqueda e inserción son directamente proporcionales a la longitud de las cadenas de caracteres
     public void inscribir(String estudiante, String carrera, String materia){
         carreras.inscribir(estudiante, carrera, materia);
         estudiantes.inscribir(estudiante);
     }
+    
     //3
+    //En el peor de los casos, se recorre hasta la ultima carrera dentro trie carrera, y luego hasta la ultima la ultima de esa carrera.
+    //La operacion de busqueda es proporcional a la longitud de la cadena(Carreras y Materias)
+    
     public int inscriptos(String materia, String carrera){
         return carreras.buscar(carrera).getMaterias().buscar(materia).inscriptos();
     }
+
+  
     //4
+    //En el peor de los casos, se recorre hasta la ultima carrera dentro trie carrera, y luego hasta la ultima la ultima de esa carrera. 
+    //Las operaciones de busqueda e inserción son directamente proporcionales a la longitud de las cadenas de caracteres
     public void agregarDocente(CargoDocente cargo, String carrera, String materia){
         carreras.agregarDocente(cargo, carrera, materia);
     }
+    
     //5
+    //En el peor de los casos, se recorre hasta la ultima carrera dentro trie carrera, y luego hasta la ultima la ultima de esa carrera.
+    //La operación de busqueda es directamente proporcional a la longitud de la cadena de caracteres(Carreras y Materias)
+  
     public int[] plantelDocente(String materia, String carrera){
         return carreras.buscar(carrera).getMaterias().buscar(materia).plantel();
     }
+
     //6
+    //En el peor de los casos, se recorre hasta la ultima carrera dentro trie carrera, y luego hasta la ultima la ultima de esa carrera.
+    //La operación de búsqueda es directamente proporcional a la longitud de la cadena de caracteres(Carreras y Materias)
+  
     public boolean excedeCupo(String materia, String carrera){
         int[] plantel = carreras.buscar(carrera).getMaterias().buscar(materia).plantel();
         int cupos = max(max(plantel[0] * 250, 100 * plantel[1]),max( 20 * plantel[2],30 * plantel[2]));
         return carreras.buscar(carrera).getMaterias().buscar(materia).inscriptos() > cupos; 
     }
+  
     //7
+    //Se recorren todas las cadenas de caracteres que representan a una carrera
+  
     public String[] carreras(){
         return carreras.get_carreras();    
     }
+  
     //8
+    //El peor de los casos depende de la cantidad de materias que tiene que cada carrera
+    //La operación de búsqueda es directamente proporcional a la longitud de la cadena de caracteres de la carrera y la longitud de la cadena de caracteres de las materias de esa carrera
+  
     public String[] materias(String carrera){
         return carreras.buscar(carrera).getMaterias().get_materias();
     }
+
     //9
+    //Cumple preguntando directamente el valor de un entero el cual siempre tiene la misma longitud, por lo tanto para cualquier entero es una longitud cosntante
+  
     public int materiasInscriptas(String estudiante){
         return estudiantes.materias_inscriptas(estudiante);
     }
+  
     //10
+    //Las operaciones de búsqueda (carrera y materia) son directamente proporcionales a la longitud de la cadena de caracteres
+    //La operación cerrar materia depende de la longitud de los nombres relacionados a esa materia y de la cantidad de estudiantes inscriptos en esa materia
     public void cerrarMateria(String materia, String carrera){
         NodoCarreras c = carreras.buscar(carrera);
         NodoMaterias m = c.getMaterias().buscar(materia);
