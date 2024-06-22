89846da45bd0679d5b16c373c94b67b3bc134c38
Santiago08 <142249223+shogek8@users.noreply.github.com>
2024-06-22 16:43:43 -0300

    Update Materia.java

--- src/main/java/aed/nuevo/Materia.java
+++ src/main/java/aed/nuevo/Materia.java
@@ -2,7 +2,7 @@ package aed.nuevo;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//Invariante de Representacion: raiz es distinto de null y raiz cumple el invariante de NodoMateria y los hijos de la raiz estan ordenados lexicograficamente y no puede tener hijos iguales.
 public class Materia {
     NodoMaterias raiz;
 
