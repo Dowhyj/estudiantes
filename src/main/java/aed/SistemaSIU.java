df673431b6b9c27b76bdbe24a1e78bfd1a3099c1
Aquilesmar <87635404+Aquilesmar@users.noreply.github.com>
2024-06-22 20:40:32 -0300

    Update SistemaSIU.java

--- src/main/java/aed/SistemaSIU.java
+++ src/main/java/aed/SistemaSIU.java
@@ -2,7 +2,7 @@ epackage aed;
 
 import java.util.ArrayList;
 //Invariante de Representacion: carreras cumple el invariante de Carreras y estudiantes cumple el invariante de Estudiante
-//para todo nodo1 hoja y nodo2 hoja pertenecientes a NodoMaterias, nodo1 = nodo2 si solo si el nombre de la materia de nodo1 y nombre de la materia del nodo2 pertenecen a un mismo InfoMateria
+//para todo nodo1 hoja y nodo2 hoja pertenecientes a NodoMaterias, los observadores del nodo1 son iguales a los observadores del nodo2 si solo si el nombre de la materia de nodo1 y nombre de la materia del nodo2 pertenecen a un mismo InfoMateria
 public class SistemaSIU {
   
 
